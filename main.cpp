55f0dfa7e0f79bb23399b01a053e0289217c60c0
Farhan Pavel <107743709+FarhanPavel@users.noreply.github.com>
2023-05-02 18:02:32 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -131,9 +131,7 @@ int main()
    cout<<"\t\t\t       ~\t"<<"\t\t\t\t\t    ~"<<"\n";
    cout<<"\t\t\t       ~\t"<<"   \t   Developed By\t    \t\t    ~"<<endl<<"\t\t\t       ~\t\t\t\t\t\t    ~"<<endl;
      cout<<"\t\t\t       ~\t"<<"   \t    ->  Pavel\t    \t\t    ~"<<endl<<"\t\t\t       ~\t\t\t\t\t\t    ~"<<endl;
-      cout<<"\t\t\t       ~\t"<<"   \t    ->  Konok\t    \t\t    ~"<<endl<<"\t\t\t       ~\t\t\t\t\t\t    ~"<<endl;
-      cout<<"\t\t\t       ~\t"<<"   \t    ->  Basak\t    \t\t    ~"<<endl<<"\t\t\t       ~\t\t\t\t\t\t    ~"<<endl;
-
+  
   cout<<"\t\t\t       ~\t     "<<"Jahangirnagar University\t\t    ~"<<endl<<"\t\t\t       ~\t   \t\t\t\t\t    ~"<<endl;
    cout<<"\t\t\t       ~\t"<<"   \t      IIT-12\t\t    \t    ~"<<endl<<"\t\t\t       ~\t   \t\t\t\t\t    ~"<<endl;
 
